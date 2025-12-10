ba27b84a3688c33b62299c318fc4dedfeec7889a
wanghao.su <wanghao.su@ccb.com>
2025-12-10 15:37:37 +0800

    _init_.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-from src.testFunctions import print_hello_actions
 from src.utils.UserUtils import choose_directory
 
 if __name__ == '__main__':
-    print_hello_actions()
     choose_dir = choose_directory()
+    print('成功执行！！！')
--- src/testFunctions.py
+++
@@ -1,2 +0,0 @@
-def print_hello_actions():
-    print('hello actions!')
