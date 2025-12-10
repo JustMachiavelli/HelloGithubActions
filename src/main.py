8bf6ba825779edc3a2efc5426c1beae58f8f02b1
wanghao.su <wanghao.su@ccb.com>
2025-12-10 14:05:55 +0800

    requirements

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from src.testFunctions import print_hello_actions
+from src.utils.UserUtils import choose_directory
 
 if __name__ == '__main__':
     print_hello_actions()
+    choose_dir = choose_directory()
